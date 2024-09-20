fc268229c46dca11dfc815b920c0c64f903e1cd5
rossjt2008 <ross.taylor@pcastudentemail.org>
2024-09-20 11:13:27 -0400

    Test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 /*
 The JS. I'm thinking let's LOCALSTORAGE *and* MYSQL the data?
 */
+var button1 = 1;
+var button2 = 2;
+var button3 = 3;
+var button4 = 4;
+var button5 = document.getElementById("customhours").value;
+var totalhours = button1+button2+button3+button4+button5;
