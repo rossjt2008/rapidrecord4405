374f46a123bd58222d74a43739b79fff5536ecda
el-javascriptor <owenjoshuabryant3@gmail.com>
2024-09-30 09:53:38 -0400

    add names yay

--- login.js
+++ login.js
@@ -2,13 +2,13 @@
 // run whenDonePressed() when it is clicked, essentially does the same thing as onclick=“whenDonePressed()” in the HTML
 function clickSubmit() {
     //add code here to manage whatever needs to be managed in the JS, saving info, etc
-    let members = ["","",""];
+    let members = ["Owen Bryant","Ross Taylor",""];
     let firstname =  document.getElementById("firstname").value
     firstname = firstname[0].toUpperCase() + firstname.substring(1);
     let lastname =  document.getElementById("lastname").value
     lastname = lastname[0].toUpperCase() + lastname.substring(1);
     let fullname = firstname + " " + lastname
-    if (fullname == "Ross Taylor") {
+    if (members.includes(fullname)) {
         //and then switch page
         localStorage.setItem("fullname", fullname)
         self.location = "index.html";  //or whichever file comes next
