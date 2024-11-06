290199d5965a94433bf3ccfbb4f0947e24556a1e
rossjt2008 <ross.taylor@pcastudentemail.org>
2024-11-06 08:38:43 -0500

    Added system access page.

---
+++ systemaccess.js
@@ -0,0 +1,44 @@
+let validpage = 0
+if (localStorage.getItem("teachername")) {
+    console.log("teachername is defined.")
+} else {
+    console.log("Instructed is not logged in, going to the login screen.")
+    validpage = 1
+}
+if (localStorage.getItem("teachername") == undefined) {
+    console.log("User is not an instructor.")
+    validpage = 1
+} else {
+    console.log("Instructor valid.")
+}
+if (localStorage.getItem("fullname")) {
+	console.log("fullname is defined.")
+} else {
+	console.log("fullname is not defined.")
+}
+if (localStorage.getItem("fullname") == undefined) {
+	console.log("User is not a student.")
+} else {
+    if (localStorage.getItem("teachername") == undefined) {
+        console.log("Student logged in. Going to hours input screen.")
+        validpage = 2
+    } else {
+        console.log("Teacher has accessed a student's hours.")
+    }
+}
+if (validpage == 1) {
+	self.location = "login.html"
+} else if (validpage == 2) {
+	self.location = "index.html"
+}
+
+let password = "PassTest"
+let passinput = null;
+let idnumber = "04405"
+let idinput = null;
+
+function clickSubmit() {
+    //add code here to manage whatever needs to be managed in the JS, saving info, etc
+    passinput = document.getElementById("password").value;
+    idinput = document.getElementById("idnumber").value;
+}
