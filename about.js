91cfb26224c97deef766c343c4c2f96c3b277bde
rossjt2008 <ross.taylor@pcastudentemail.org>
2024-11-13 12:20:23 -0500

    Added savedatabase and fixed about & website files

--- about.js
+++ about.js
@@ -1,3 +1,18 @@
+getlastpage()
+function getlastpage() {
+    lastpage = localStorage.getItem("lastpage")
+    if (lastpage == "login.html") {
+        console.log("lastpage is login.html")
+    } else if (lastpage == "submitted.html") {
+        console.log("lastpage is submitted.html")
+        document.getElementById("welcome").textContent = "Account Options"
+    } else if (lastpage == "teacheroptions.html") {
+        console.log("lastpage is teacheroptions.html")
+        document.getElementById("welcome").textContent = "Options & Tools"
+    } else {
+        localStorage.setItem("lastpage", "login.html")
+    }
+}
 function websites() {
     self.location = "websites.html"
 }
